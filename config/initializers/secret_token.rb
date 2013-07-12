64952ae833daf56dacbe5a3a28141a7d0bf35059
Jaden <malcolm-ding@hotmail.com>
2013-07-13 08:21:07 +1200

    TDD

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TDD::Application.config.secret_token = 'c72f0ba25b94e78c03ebd961350aa7cef2a2a98e6b4962839b7fc449537b1a9219e1fcb52ad9b8e032bb4d76664f3ce9104d286edac7a0f68dec2df44d70d542'
